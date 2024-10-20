ad00702ca70e70a1d20c5f00aa092dc6aaecb132
comfortmedic <comfortmedic247@gmail.com>
2024-10-20 16:02:22 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,4 +6,30 @@ $(document).ready(function() {
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
     });
+
+    // Full page scroll behavior
+    let sections = $('section');
+    let currentIndex = 0;
+
+    $(window).on('wheel', function(e) {
+        if ($('html, body').is(':animated')) {
+            return;
+        }
+
+        if (e.originalEvent.deltaY > 0) {
+            // Scroll down
+            if (currentIndex < sections.length - 1) {
+                currentIndex++;
+            }
+        } else {
+            // Scroll up
+            if (currentIndex > 0) {
+                currentIndex--;
+            }
+        }
+
+        $('html, body').animate({
+            scrollTop: $(sections[currentIndex]).offset().top
+        }, 800);
+    });
 });
